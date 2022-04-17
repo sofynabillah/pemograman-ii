f6d353150409d7240f4485538ffb13c48f9aefdd
sofynabillah <101787852+sofynabillah@users.noreply.github.com>
2022-04-17 22:06:02 +0700

    commit2

---
+++ Tugas 5 PII - Sofy Nabillah - 20200121028 - SI Bisnis 1.4/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CobaController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/coba', function () {
+    return view('coba');
+});
+Route::get('/coba/{no}', [CobaController::class, 'coba']); 
+Route::get('/test', [CobaController::class, 'index']); 
+Route::get('/test/{ke}', [CobaController::class, 'urutan']);
+
+*/
+
+Route::get('', [CobaController::class, 'index']);
+Route::get('/friends', [CobaController::class, 'index']);
+Route::get('/friends/create', [CobaController::class, 'create']);
+Route::post('/friends', [CobaController::class, 'store']);
